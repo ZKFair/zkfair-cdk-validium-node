ebb2d265644d0314097c833f228eced6fea17e60
bros <arnaubennassar5@gmail.com>
2023-08-23 11:49:44 +0000

    Fix nil when err on CalculateTxBreakEvenGasPrice

--- sequencer/finalizer.go
+++ sequencer/finalizer.go
@@ -571,14 +571,13 @@ func (f *finalizer) processTransaction(ctx context.Context, tx *TxTracker) (errW
 					return nil, err
 				} else {
 					log.Warnf("EffectiveGasPrice is disabled, but failed to calculate BreakEvenGasPrice: %s", err)
-					tx.BreakEvenGasPrice = big.NewInt(0)
 				}
 			}
 		}
 
 		effectivePercentage := state.MaxEffectivePercentage
 
-		if tx.BreakEvenGasPrice.Uint64() != 0 {
+		if tx.BreakEvenGasPrice != nil && tx.BreakEvenGasPrice.Uint64() != 0 {
 			// If the tx gas price is lower than the break even gas price, we process the tx with the user gas price (100%)
 			if tx.GasPrice.Cmp(tx.BreakEvenGasPrice) <= 0 {
 				tx.IsEffectiveGasPriceFinalExecution = true
