9957f09a8e9696ee033e7cf345a0695935d05e80
bros <arnaubennassar5@gmail.com>
2023-08-30 14:31:47 +0000

    Fix datacommittee e2e test

--- test/e2e/datacommittee_test.go
+++ test/e2e/datacommittee_test.go
@@ -114,6 +114,8 @@ func TestDataCommittee(t *testing.T) {
 			WsURL:                "ws://cdk-validium-mock-l1-network:8546",
 			CDKValidiumAddress:   operations.DefaultL1CDKValidiumSmartContract,
 			DataCommitteeAddress: operations.DefaultL1DataCommitteeContract,
+			Timeout:              cTypes.Duration{time.Second},
+			RetryPeriod:          cTypes.Duration{time.Second},
 		},
 		PrivateKey: cTypes.KeystoreFileConfig{
 			Path:     ksFile,
